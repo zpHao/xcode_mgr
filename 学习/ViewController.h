251569e79ea2617599698f490f529540b638ce8a
haozp <haozp@haozpdemacbook-pro.local>
2017-04-10 10:41:19 +0800

    Initial Commit

---
+++ 学习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  学习
+//
+//  Created by haozp on 17/4/10.
+//  Copyright © 2017年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 学习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  学习
+//
+//  Created by haozp on 17/4/10.
+//  Copyright © 2017年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
